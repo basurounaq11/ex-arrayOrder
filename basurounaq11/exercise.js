e8adb55e9e6964c379f85f27061e7c56513f3891
basurounaq11 <basurounaq11@gmail.com>
2018-02-16 11:23:44 -0500

    finished exercise

---
+++ basurounaq11/exercise.js
@@ -0,0 +1,15 @@
+var exercise = {};
+
+exercise.order = function(unordered) {
+    var ordered = [];
+
+    // Sorting a numerical array from minimum to maximum
+    ordered = unordered.sort(function(a,b){return a-b});
+
+    return ordered;
+};
+
+var unordered = [11, 2, 15, 4, 5, 20, 7, 17, 13, 10, 1, 12, 9, 14, 3, 16, 8, 18, 19, 6];
+exercise.order(unordered);
+
+module.exports = exercise;
